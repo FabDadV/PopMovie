813091e67d4f681b477fca8631e801836446420d
FabDadV <fabdadv@gmail.com>
2018-04-04 14:24:40 +0700

    Creating DB FavMovie

---
+++ app/src/main/java/com/ex/popmovie/data/DatabaseContract.java
@@ -0,0 +1,13 @@
+package com.ex.popmovie.data;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+    // Create Movie table:
+    public class MovieTable implements BaseColumns {
+        // Create table name and each of the db columns
+
+
+    }
+
+}
