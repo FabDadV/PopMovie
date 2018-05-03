a6ac81e43aead131e0331d6b0ef51d1113caa99c
Nick <fabdadv@gmai.com>
2018-05-03 15:46:55 +0700

    Debug SQLite wuth  Stetho

--- app/src/main/java/com/ex/popmovie/App.java
+++ app/src/main/java/com/ex/popmovie/App.java
@@ -2,5 +2,15 @@ package com.ex.popmovie;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 public class App extends Application {
+    private static App instance;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        Stetho.initializeWithDefaults(this);
+    }
+
 }
